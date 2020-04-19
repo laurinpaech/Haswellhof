3e17ed1c08661b1fd9c4ddf907bb4c4b40ae66dc
@volflow <valentin@valentin-wolf.de>
2020-04-19 21:12:20 +0200

    integral images WIP

--- descriptor.c
+++ descriptor.c
@@ -11,18 +11,15 @@ void get_gaussian(float sigma, int size, float* dest) {
     }
 }
 
-void get_descriptor(float* patch, int scale, int PATCH_SIZE, *float descriptor) {
-    // Rescale patch s.t. scale=1 or assume that this was handeled before
-    // OR use integral image!!!
-    // not sure what is more performant
-    // TODO
+void get_descriptor(int keyp_x, int keyp_y, int scale, float* iimage, int height, int width, int PATCH_SIZE, *float descriptor) {
 
-    // now we can assume scale=1
-    // which makes wavelet responses trivial to compute
+    // assert keyp_x, keyp_y is far enough out
+
+    int top_right = keyp_y/2*width + keyp_x/2; // TODO: check if  indicescorrect
 
     // compute/load gaussian weighting matrix GW for PATCH_SIZE here 
     // due to resue over all patches we probably want to precompute it and pass it as a param 
-    GW = (float*) malloc(PATCH_SIZE*PATCH_SIZE * sizeof(floar));
+    GW = (float*) malloc(PATCH_SIZE*PATCH_SIZE * sizeof(float));
     get_gaussian(3.3, PATCH_SIZE, GW);
 
     // store wavelet responses
@@ -30,11 +27,31 @@ void get_descriptor(float* patch, int scale, int PATCH_SIZE, *float descriptor) 
     float dy[PATCH_SIZE][PATCH_SIZE];
 
     // compute haar wavelet responses
-    for (int i=0, i<PATCH_SIZE, i++) {
-        for (int j=0, j<PATCH_SIZE, j++) {
+    for (int i=0, i<PATCH_SIZE, i++) { // x coordinate
+        for (int j=0, j<PATCH_SIZE, j++) { // y coordinate
             float gw = GW[i*PATCH_SIZE + j];
-            dx[i][j] = gw*(patch[(i+1)*PATCH_SIZE + j] - patch[i*PATCH_SIZE + j] + patch[(i+1)*PATCH_SIZE + j+1] - patch[i*PATCH_SIZE + j+1]);
-            dy[i][j] = gw*(patch[i*PATCH_SIZE + j+1] - patch[i*PATCH_SIZE + j] + patch[(i+1)*PATCH_SIZE + j+1] - patch[(i+1)*PATCH_SIZE + j]);
+
+            // compute needed corners of the 2sx2s patch
+            // c1 c2 c3 
+            // c4 ij c5
+            // c6 c7 c8
+
+            // TODO: check if  indicescorrect
+            // int m = (j*width+i)*scale;
+            int c1 = top_right + ((j)*scale-1)*width+((i)*scale-1);
+            int c2 = top_right + ((j)*scale-1)*width+((i+1)*scale-1);
+            int c3 = top_right + ((j)*scale-1)*width+((i+2)*scale-1);
+            int c4 = top_right + ((j+1)*scale-1)*width+((i)*scale-1);
+            int c6 = top_right + ((j+2)*scale-1)*width+((i)*scale-1);
+            int c5 = top_right + ((j+1)*scale-1)*width+((i+2)*scale-1);
+            int c7 = top_right + ((j+2)*scale-1)*width+((i+1)*scale-1);
+            int c8 = top_right + ((j+2)*scale-1)*width+((i+2)*scale-1);
+
+            dx[i][j] = gw*(iimages[c8] + iimages[c4] - iimages[c5] - iimages[c6] - (iimages[c5] + iimages[c1] - iimages[c3] - iimages[c4]));
+            dy[i][j] = gw*(iimages[c8] + iimages[c2] - iimages[c3] - iimages[c7] - (iimages[c7] + iimages[c1] - iimages[c2] - iimages[c6]));
+
+            // dx[i][j] = gw*(patch[(i+1)*PATCH_SIZE + j] - patch[i*PATCH_SIZE + j] + patch[(i+1)*PATCH_SIZE + j+1] - patch[i*PATCH_SIZE + j+1]);
+            // dy[i][j] = gw*(patch[i*PATCH_SIZE + j+1] - patch[i*PATCH_SIZE + j] + patch[(i+1)*PATCH_SIZE + j+1] - patch[(i+1)*PATCH_SIZE + j]);
         }
     }
 
@@ -58,7 +75,8 @@ void get_descriptor(float* patch, int scale, int PATCH_SIZE, *float descriptor) 
                     descriptor[desc_idx+3] += (float)fabs(y); // sum(abs(y))
                 }   
             }
-
+            
+            // precompute for normaliztion
             for (int m=0; m<4; m++) 
                 sum_of_squares += descriptor[m]*descriptor[m];
 
