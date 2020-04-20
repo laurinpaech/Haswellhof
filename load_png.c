a4df6a4546c66fe43779e0771cbb92bdb9e43770
Carla <cjancik@student.ethz.ch>
2020-04-20 17:46:07 +0200

    load image as grayscale

--- load_png.c
+++ load_png.c
@@ -2,20 +2,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// https://github.com/lvandeve/lodepng
+/** https://github.com/lvandeve/lodepng */
 unsigned char* decodeOneStep(const char* filename) {
 	unsigned error;
 	unsigned char* image = 0;
 	unsigned width, height;
 
-	error = lodepng_decode32_file(&image, &width, &height, filename);
+	/*error = lodepng_decode32_file(&image, &width, &height, filename);*/
+	error = lodepng_decode_file(&image, &width, &height, filename, LCT_GREY, 8);
 
 	if (error)  {
 		printf("error %u: %s\n", error, lodepng_error_text(error));
 	}
 
 	printf("width: %d; height: %d\n%u %u %u %u\n", width, height, image[0], image[1], image[2], image[3]);
-
+	/*convert image to grayscale*/
 	return image;
 }
 
@@ -23,7 +24,7 @@ int main(int argc, char const *argv[])
 {
 	const char* filename = argc > 1 ? argv[1] : "test.png";
 
-	// image in memory: RGBA RGBA RGBA RGBA RGBA
+	/* image in memory: RGBA RGBA RGBA RGBA RGBA */
 	unsigned char* image = decodeOneStep(filename);
 
 	/* use image here */
