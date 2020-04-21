47fea20957b60974a83cde53070551456a2b71c4
Sebastian Winberg <winbergs@student.ethz.ch>
2020-04-21 18:19:52 +0200

    Bug fixed box_integral implementation

--- integral_image.h
+++ integral_image.h
@@ -12,13 +12,14 @@ struct integral_image {
 float box_integral(struct integral_image *iimage, int row, int col, int rows, int cols) {
 
     float *data = (float *) iimage->data;
-    int step = 1;
+    int width = iimage->width;
+    int height = iimage->height;
 
     // subtracting by one for row/col because row/col is inclusive.
-    int r0 = fmin(row, iimage->height) - 1;
-    int c0 = fmin(col, iimage->width) - 1;
-    int r1 = fmin(row + rows, iimage->height) - 1;
-    int c1 = fmin(col + cols, iimage->width) - 1;
+    int r0 = fmin(row, height) - 1;
+    int c0 = fmin(col, width) - 1;
+    int r1 = fmin(row + rows, height) - 1;
+    int c1 = fmin(col + cols, width) - 1;
 
     float A = 0.0f;
     float B = 0.0f; 
@@ -26,16 +27,16 @@ float box_integral(struct integral_image *iimage, int row, int col, int rows, in
     float D = 0.0f;
 
     if (r0 >= 0 && c0 >= 0) {
-        A = data[r0 * step + c0];
+        A = data[r0 * width + c0];
     }
     if (r0 >= 0 && c1 >= 0) {
-        B = data[r0 * step + c1];
+        B = data[r0 * width + c1];
     }  
     if (r1 >= 0 && c0 >= 0) {
-        C = data[r1 * step + c0];
+        C = data[r1 * width + c0];
     }
     if (r1 >= 0 && c1 >= 0) {
-        D = data[r1 * step + c1];
+        D = data[r1 * width + c1];
     }
 
     return fmax(0.0f, A - B - C + D);
