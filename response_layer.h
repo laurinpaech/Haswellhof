5dcbd3072b6313d0e8b0a9b3359df7ac7fa462b8
Sebastian Winberg <winbergs@student.ethz.ch>
2020-04-22 20:42:47 +0200

    Added get_laplacian and get_response functionality to response layers

--- response_layer.h
+++ response_layer.h
@@ -22,3 +22,25 @@ struct response_layer {
     bool *laplacian;
 
 };
+
+inline bool get_laplacian(struct response_layer *rl, int row, int col) {
+    assert(rl != NULL && rl->laplacian != NULL);
+    return rl->laplacian[row * rl->width + col];
+}
+
+inline bool get_laplacian(struct response_layer *rl, int row, int col, struct response_layer *src) {
+    assert(rl != NULL && rl->laplacian != NULL && src != NULL);
+    int scale = rl->width / src->width;
+    return rl->laplacian[(scale * row) * rl->width + (scale * col)];
+}
+
+inline float get_response(struct response_layer *rl, int row, int col) {
+    assert(rl != NULL && rl->response != NULL);
+    return rl->response[row * rl->width + col];
+}
+
+inline float get_response(struct response_layer *rl, int row, int col, struct response_layer *src) {
+    assert(rl != NULL && rl->response != NULL && src != NULL);
+    int scale = rl->width / src->width;
+    return rl->response[(scale * row) * rl->width + (scale * col)];
+}
