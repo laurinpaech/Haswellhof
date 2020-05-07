fce66ccc74d3a54b6778a82a3b25b21d6d7b764a
@volflow <valentin@valentin-wolf.de>
2020-05-07 22:46:08 +0200

    inline haarXY function

--- src/optimization/descriptor_opt.cpp
+++ src/optimization/descriptor_opt.cpp
@@ -1,4 +1,5 @@
 #include "descriptor.h"
+#include "descriptor_opt.h"
 #include "integral_image.h"
 #include "interest_point.h"
 
@@ -8,22 +9,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define CONTINUE_AT_BOUNDARY 0
+#define PRECHECK_BOUNDARIES 1
 
 #define MAX(a,b) (((a)>(b))?(a):(b))
 #define MIN(a,b) (((a)<(b))?(a):(b))
 
-void get_descriptor_opt(struct integral_image* iimage, struct interest_point* ipoint, float* GW) {
+void get_descriptor_inlinedHaarWavelets(struct integral_image* iimage, struct interest_point* ipoint, float* GW) {
 
     float scale = ipoint->scale;
     // TODO: (Sebastian) Is this correct with  "- 0.5"?
     int ipoint_x = (int) (ipoint->x + 0.5);
     int ipoint_y = (int) (ipoint->y + 0.5);
 
-    int step = MAX((int)(scale/2 + 0.5),1); // rounding is done this way in the original implementaion
+    int step = MAX((int)(scale + 0.5),1); // rounding is done this way in the original implementaion
 
-    int col_offset = ipoint_x-step*11; //10 - 1 = PATCH_SIZE/2 + (shift to obtain upper left corner of haar wavelet filter)
-    int row_offset = ipoint_y-step*11;
+    int col_offset = ipoint_x-step*10 - step/2;
+    int row_offset = ipoint_y-step*10 - step/2;
 
     // build descriptor
     float* descriptor = ipoint->descriptor;
@@ -40,6 +41,7 @@ void get_descriptor_opt(struct integral_image* iimage, struct interest_point* ip
             float sum_y = 0.0f;
             float abs_x = 0.0f;
             float abs_y = 0.0f;
+            // float missed_scaling = 1;
             for (int k=i*5; k<i*5+5; ++k) {
                 for (int l=j*5; l<j*5+5; ++l) { 
                     // iterate over 5x5 sample points of sub_patch[i][j]
@@ -50,83 +52,16 @@ void get_descriptor_opt(struct integral_image* iimage, struct interest_point* ip
                     // compute left corner of the [2*scale x 2*scale] patch:
                     int row = row_offset + l*step;
                     int col = col_offset + k*step;
-#if !CONTINUE_AT_BOUNDARY
-                    // subtracting by one for row/col because row/col is inclusive.
-                    int r0 = MIN(row, height) - 1;         
-                    int c0 = MIN(col, width) - 1;         
-                    int r1 = MIN(row + step, height) - 1;  
-                    int c1 = MIN(col + step, width) - 1;   
-                    int r2 = MIN(row + 2*step, height) - 1;
-                    int c2 = MIN(col + 2*step, width) - 1;
 
-                    float r0c0 = 0.0f; // A
-                    float r0c1 = 0.0f; // B
-                    float r0c2 = 0.0f; // C
-                    float r1c0 = 0.0f; // D
-                    float r1c2 = 0.0f; // E
-                    float r2c0 = 0.0f; // F
-                    float r2c1 = 0.0f; // G
-                    float r2c2 = 0.0f; // H
+                    float haarX = 0.0f;
+                    float haarY = 0.0f;
 
-                    if (r0 >= 0 && c0 >= 0) {
-                        r0c0 = data[r0 * width + c0];
-                    }
-                    if (r0 >= 0 && c1 >= 0) {
-                        r0c1 = data[r0 * width + c1];
-                    }
-                    if (r0 >= 0 && c2 >= 0) {
-                        r0c2 = data[r0 * width + c2];
-                    }
-                    if (r1 >= 0 && c0 >= 0) {
-                        r1c0 = data[r1 * width + c0];
-                    }
-                    if (r1 >= 0 && c2 >= 0) {
-                        r1c2 = data[r1 * width + c2];
-                    }
-                    if (r2 >= 0 && c0 >= 0) {
-                        r2c0 = data[r2 * width + c0];
-                    }
-                    if (r2 >= 0 && c1 >= 0) {
-                        r2c1 = data[r2 * width + c1];
-                    }
-                    if (r2 >= 0 && c2 >= 0) {
-                        r2c2 = data[r2 * width + c2];
-                    }
+                    // TODO: (valentin) can parameter passing be done better?
+#if (!PRECHECK_BOUNDARIES)
+                    haarXY(data, height, width, row, col, step, &haarX, &haarY);
 #else
-                    if (row <= 0 || col <= 0 || row + 2*step > height || col + 2*step > width) {
-                        // wavelets that can not be applied completely will be skipped
-                        // this is not just more efficeint but I believe may also be more accurate
-                        // as cut off wavelet responses can distort features
-                        printf("test\n");
-                        continue;
-                    }
-
-                    int r0 = row - 1;         
-                    int c0 = col - 1;         
-                    int r1 = row + step - 1;  
-                    int c1 = col + step - 1;   
-                    int r2 = row + 2*step - 1;
-                    int c2 = col + 2*step - 1;
-
-                    float r0c0 = data[r0 * width + c0];
-                    float r0c1 = data[r0 * width + c1];
-                    float r0c2 = data[r0 * width + c2];
-                    float r1c0 = data[r1 * width + c0];
-                    float r1c2 = data[r1 * width + c2];
-                    float r2c0 = data[r2 * width + c0];
-                    float r2c1 = data[r2 * width + c1];
-                    float r2c2 = data[r2 * width + c2];
-                    
+                    haarXY_precheck_boundaries(data, height, width, row, col, step, &haarX, &haarY);
 #endif
-                    // float haarX = (r0c0 - r0c1 - r2c0 + r2c1) - (r0c1 - r0c2 - r2c1 + r2c2);
-                    // float haarY = (r0c0 - r0c2 - r1c0 + r1c2) - (r1c0 - r1c2 - r2c0 + r2c2);
-
-                    float r0c0_sub_r2c2 = r0c0 - r2c2;
-                    float r0c2_sub_r2c0 = r0c2 - r2c0;
-
-                    float haarX = r0c0_sub_r2c2 - 2*(r0c1 - r2c1) + r0c2_sub_r2c0;
-                    float haarY = r0c0_sub_r2c2 - 2*(r1c0 - r1c2) - r0c2_sub_r2c0;
-
                     // haarX
                     float x = gw * haarX;
                     float y = gw * haarY;
@@ -135,8 +70,21 @@ void get_descriptor_opt(struct integral_image* iimage, struct interest_point* ip
                     sum_y += y; // sum(y)
                     abs_x += fabs(x); // sum(abs(x))
                     abs_y += fabs(y); // sum(abs(y))
+
+                    if (row <= 0 
+                        || col <= 0 
+                        || (row + 2*step) > height 
+                        || (col + 2*step) > width) {
+                        // wavelets that can not be applied completely will be skipped
+                        // this is not just more efficeint but I believe may also be more accurate
+                        // as cut off wavelet responses can distort features
+                        // printf("%f %f\n", x, y);
+                        continue;
+                    }
+
                 }
             }
+
             descriptor[desc_idx] = sum_x;
             descriptor[desc_idx+1] = sum_y;   
             descriptor[desc_idx+2] = abs_x;
@@ -162,121 +110,130 @@ void get_descriptor_opt(struct integral_image* iimage, struct interest_point* ip
 }
 
 
-// void get_msurf_descriptor(struct integral_image* iimage, struct interest_point* ipoint) {
+void get_msurf_descriptor_inlinedHaarWavelets(struct integral_image* iimage, struct interest_point* ipoint) {
 
-//     float scale = ipoint->scale;
-//     int ipoint_x = (int) round(ipoint->x);
-//     int ipoint_y = (int) round(ipoint->y);
+    float scale = ipoint->scale;
+    int ipoint_x = (int) round(ipoint->x);
+    int ipoint_y = (int) round(ipoint->y);
 
-//     // build descriptor
-//     float* descriptor = ipoint->descriptor;
-//     int desc_idx = 0;
-//     float sum_of_squares = 0.0f;
+    float *data = (float *)iimage->data;
+    int width = iimage->width;
+    int height = iimage->height;
+
+    // build descriptor
+    float* descriptor = ipoint->descriptor;
+    int desc_idx = 0;
+    float sum_of_squares = 0.0f;
     
-//     // subregion centers for the 4x4 gaussian weighting
-//     float cx = -0.5f;
-//     float cy = 0.0f;
+    // subregion centers for the 4x4 gaussian weighting
+    float cx = -0.5f;
+    float cy = 0.0f;
 
-//     //float co = 1;
-//     //float si = 0;
+    //float co = 1;
+    //float si = 0;
 
-//     int i = -8;
+    int i = -8;
 
-//     // calculate descriptor for this interest point
-//     while(i < 12) {
+    // calculate descriptor for this interest point
+    while(i < 12) {
 
-//         int j = -8;
-//         i = i - 4;
+        int j = -8;
+        i = i - 4;
 
-//         cx += 1.0f;
-//         cy = -0.5f;
+        cx += 1.0f;
+        cy = -0.5f;
 
-//         while(j < 12) {
+        while(j < 12) {
             
-//             float dx = 0.0f;
-//             float dy = 0.0f; 
-//             float mdx = 0.0f; 
-//             float mdy = 0.0f;
+            float dx = 0.0f;
+            float dy = 0.0f; 
+            float mdx = 0.0f; 
+            float mdy = 0.0f;
 
-//             cy += 1.0f;
+            cy += 1.0f;
 
-//             j = j - 4;
+            j = j - 4;
 
-//             //int xs = (int) round(ipoint_x + ( -jx*scale*si + ix*scale*co));
-//             //int ys = (int) round(ipoint_y + ( jx*scale*co + ix*scale*si));
-//             int xs = (int) round(ipoint_x + (i + 5) * scale);
-//             int ys = (int) round(ipoint_y + (j + 5) * scale);
+            //int xs = (int) round(ipoint_x + ( -jx*scale*si + ix*scale*co));
+            //int ys = (int) round(ipoint_y + ( jx*scale*co + ix*scale*si));
+            int xs = (int) round(ipoint_x + (i + 5) * scale);
+            int ys = (int) round(ipoint_y + (j + 5) * scale);
 
-//             for (int k = i; k < i + 9; ++k) {
-//                 for (int l = j; l < j + 9; ++l) {
+            for (int k = i; k < i + 9; ++k) {
+                for (int l = j; l < j + 9; ++l) {
 
-//                     //Get coords of sample point on the rotated axis
-//                     //int sample_x = (int) round(ipoint_x + (-l*scale*si + k*scale*co));
-//                     //int sample_y = (int) round(ipoint_y + ( l*scale*co + k*scale*si));
-//                     int sample_x = (int) round(ipoint_x + k * scale);
-//                     int sample_y = (int) round(ipoint_y + l * scale);
+                    //Get coords of sample point on the rotated axis
+                    //int sample_x = (int) round(ipoint_x + (-l*scale*si + k*scale*co));
+                    //int sample_y = (int) round(ipoint_y + ( l*scale*co + k*scale*si));
+                    int sample_x = (int) round(ipoint_x + k * scale);
+                    int sample_y = (int) round(ipoint_y + l * scale);
 
-//                     //Get the gaussian weighted x and y responses
-//                     //float gauss_s1 = gaussian(xs-sample_x, ys-sample_y, 2.5f * scale);
-//                     //(1.0f/(2.0f*pi*sig*sig)) * exp( -(x*x+y*y)/(2.0f*sig*sig))
-//                     float g_factor = 0.08f / (scale*scale); // since 0.08f / (scale*scale) == 1.0f / (2.0f * 2.5f * scale * 2.5f * scale)
-//                     float g_x = xs - sample_x;
-//                     float g_y = ys - sample_y;
-//                     float gauss_s1 = M_1_PI * g_factor * exp(-g_factor * (g_x*g_x + g_y*g_y));
+                    //Get the gaussian weighted x and y responses
+                    //float gauss_s1 = gaussian(xs-sample_x, ys-sample_y, 2.5f * scale);
+                    //(1.0f/(2.0f*pi*sig*sig)) * exp( -(x*x+y*y)/(2.0f*sig*sig))
+                    float g_factor = 0.08f / (scale*scale); // since 0.08f / (scale*scale) == 1.0f / (2.0f * 2.5f * scale * 2.5f * scale)
+                    float g_x = xs - sample_x;
+                    float g_y = ys - sample_y;
+                    float gauss_s1 = M_1_PI * g_factor * exp(-g_factor * (g_x*g_x + g_y*g_y));
                     
-//                     //float rx = haarX(sample_y, sample_x, (int) 2.0 * round(scale));
-//                     //float ry = haarY(sample_y, sample_x, (int) 2.0 * round(scale));
-//                     int s = (int) round(scale);
-//                     float rx = gauss_s1 * box_integral(iimage, sample_y-s, sample_x, 2*s, s) - box_integral(iimage, sample_y-s, sample_x-s, 2*s, s);
-//                     float ry = gauss_s1 * box_integral(iimage, sample_y, sample_x-s, s, 2*s) - box_integral(iimage, sample_y-s, sample_x-s, s, 2*s);
+                    //float rx = haarX(sample_y, sample_x, (int) 2.0 * round(scale));
+                    //float ry = haarY(sample_y, sample_x, (int) 2.0 * round(scale));
+                    int s = (int) round(scale);
+                    float rx = 0.0f; //box_integral(iimage, sample_y-s, sample_x, 2*s, s) - box_integral(iimage, sample_y-s, sample_x-s, 2*s, s);
+                    float ry = 0.0f; //box_integral(iimage, sample_y, sample_x-s, s, 2*s) - box_integral(iimage, sample_y-s, sample_x-s, s, 2*s);
+
+#if (!PRECHECK_BOUNDARIES)
+                    haarXY(data, height, width, sample_y-s, sample_x-s, s, &rx, &ry);
+#else
+                    haarXY_precheck_boundaries(data, height, width, sample_y-s, sample_x-s, s, &rx, &ry);
+#endif
                     
-//                     //Get the gaussian weighted x and y responses on rotated axis
-//                     //float rrx = gauss_s1*(-rx*si + ry*co);
-//                     //float rry = gauss_s1*(rx*co + ry*si);
-//                     // TODO: (Sebastian) This seems weird...
-//                     float rrx = gauss_s1 * (ry);
-//                     float rry = gauss_s1 * (rx);
+                    //Get the gaussian weighted x and y responses on rotated axis
+                    //float rrx = gauss_s1*(-rx*si + ry*co);
+                    //float rry = gauss_s1*(rx*co + ry*si);
+                    float rrx = gauss_s1 * (ry);
+                    float rry = gauss_s1 * (rx);
 
-//                     dx += rrx;
-//                     dy += rry;
-//                     mdx += fabs(rrx);
-//                     mdy += fabs(rry);
+                    dx += rrx;
+                    dy += rry;
+                    mdx += fabs(rrx);
+                    mdy += fabs(rry);
 
-//                 }
-//             }
+                }
+            }
 
-//             // TODO: (Sebastian) Precompute this...
-//             //float gauss_s2 = gaussian(cx-2.0f,cy-2.0f,1.5f);
-//             //(1.0f/(2.0f*pi*sig*sig)) * exp( -(x*x+y*y)/(2.0f*sig*sig));
-//             float g_factor = 1.0f / 4.5f; // since 1.0f / 4.5f == 1.0f / (2.0f * 1.5f * 1.5f)
-//             float g_x = cx - 2.0f;
-//             float g_y = cy - 2.0f;
-//             float gauss_s2 = M_1_PI * g_factor * exp(-g_factor * (g_x*g_x + g_y*g_y));
+            // TODO: (Sebastian) Precompute this...
+            //float gauss_s2 = gaussian(cx-2.0f,cy-2.0f,1.5f);
+            //(1.0f/(2.0f*pi*sig*sig)) * exp( -(x*x+y*y)/(2.0f*sig*sig));
+            float g_factor = 1.0f / 4.5f; // since 1.0f / 4.5f == 1.0f / (2.0f * 1.5f * 1.5f)
+            float g_x = cx - 2.0f;
+            float g_y = cy - 2.0f;
+            float gauss_s2 = M_1_PI * g_factor * exp(-g_factor * (g_x*g_x + g_y*g_y));
 
-//             // add the values to the descriptor vector
-//             descriptor[desc_idx] = dx * gauss_s2;
-//             descriptor[desc_idx+1] = dy * gauss_s2;
-//             descriptor[desc_idx+2] = mdx * gauss_s2;
-//             descriptor[desc_idx+3] = mdy * gauss_s2;
+            // add the values to the descriptor vector
+            descriptor[desc_idx] = dx * gauss_s2;
+            descriptor[desc_idx+1] = dy * gauss_s2;
+            descriptor[desc_idx+2] = mdx * gauss_s2;
+            descriptor[desc_idx+3] = mdy * gauss_s2;
 
-//             // precompute for normaliztion
-//             sum_of_squares += (dx*dx + dy*dy + mdx*mdx + mdy*mdy) * gauss_s2*gauss_s2;
+            // precompute for normaliztion
+            sum_of_squares += (dx*dx + dy*dy + mdx*mdx + mdy*mdy) * gauss_s2*gauss_s2;
 
-//             desc_idx += 4;
+            desc_idx += 4;
 
-//             j += 9;
+            j += 9;
 
-//         }
+        }
 
-//         i += 9;
+        i += 9;
 
-//     }
+    }
 
-//     // rescale to unit vector
-//     float norm_factor = 1.0f / sqrt(sum_of_squares);
+    // rescale to unit vector
+    float norm_factor = 1.0f / sqrt(sum_of_squares);
 
-//     for (int i = 0; i < 64; ++i) {
-//         descriptor[i] *= norm_factor;
-//     }
+    for (int i = 0; i < 64; ++i) {
+        descriptor[i] *= norm_factor;
+    }
 
-// }
+}
---
+++ src/optimization/descriptor_opt.h
@@ -0,0 +1,118 @@
+#pragma once 
+
+#include <math.h>
+#include <stdlib.h>
+#include "integral_image.h"
+#include "interest_point.h"
+
+#define MIN(a,b) (((a)<(b))?(a):(b))
+
+// #define PATCH_SIZE 20
+
+// static inline float* get_gaussian(float sigma) {
+//     /* computes matrix of shape (size x size) containing prob values of
+//     2d gaussian with std=sigma and mean=(size/2, size/2) */
+//     int size = PATCH_SIZE;
+//     float* GW = (float*) malloc(size*size * sizeof(float));
+
+//     float variance = sigma*sigma;
+//     float normalization = 1/(2*M_PI*variance);
+//     for (int i=0; i<size; i++) {
+//         for (int j=0; j<size; j++) {
+//             GW[j*size + i] = normalization * exp(-((i-size/2)*(i-size/2)+(j-size/2)*(j-size/2))/(2*variance));
+//         }
+//     }
+
+//     return GW;
+// }
+
+void get_descriptor_inlinedHaarWavelets(struct integral_image* iimage, struct interest_point* ipoint, float* GW);
+
+void get_msurf_descriptor_inlinedHaarWavelets(struct integral_image* iimage, struct interest_point* ipoint);
+
+static inline void haarXY(float* ii_data, int height, int width, int row, int col, int scale, float* haarX, float* haarY) {
+    // subtracting by one for row/col because row/col is inclusive.
+    int r0 = MIN(row, height) - 1;         
+    int c0 = MIN(col, width) - 1;         
+    int r1 = MIN(row + scale, height) - 1;  
+    int c1 = MIN(col + scale, width) - 1;   
+    int r2 = MIN(row + 2*scale, height) - 1;
+    int c2 = MIN(col + 2*scale, width) - 1;
+
+    float r0c0 = 0.0f; // A
+    float r0c1 = 0.0f; // B
+    float r0c2 = 0.0f; // C
+    float r1c0 = 0.0f; // D
+    float r1c2 = 0.0f; // E
+    float r2c0 = 0.0f; // F
+    float r2c1 = 0.0f; // G
+    float r2c2 = 0.0f; // H
+
+    if (r0 >= 0 && c0 >= 0)  {
+        r0c0 = ii_data[r0 * width + c0];
+    }
+    if (r0 >= 0 && c1 >= 0) {
+        r0c1 = ii_data[r0 * width + c1];
+    }
+    if (r0 >= 0 && c2 >= 0) {
+        r0c2 = ii_data[r0 * width + c2];
+    }
+    if (r1 >= 0 && c0 >= 0) {
+        r1c0 = ii_data[r1 * width + c0];
+    }
+    if (r1 >= 0 && c2 >= 0) {
+        r1c2 = ii_data[r1 * width + c2];
+    }
+    if (r2 >= 0 && c0 >= 0) {
+        r2c0 = ii_data[r2 * width + c0];
+    }
+    if (r2 >= 0 && c1 >= 0) {
+        r2c1 = ii_data[r2 * width + c1];
+    }
+    if (r2 >= 0 && c2 >= 0) {
+        r2c2 = ii_data[r2 * width + c2];
+    }
+
+    float r0c0_sub_r2c2 = r0c0 - r2c2;
+    float r0c2_sub_r2c0 = r0c2 - r2c0;
+
+    *haarX = -1*(r0c0_sub_r2c2 - 2*(r0c1 - r2c1) + r0c2_sub_r2c0);
+    *haarY = -1*(r0c0_sub_r2c2 - 2*(r1c0 - r1c2) - r0c2_sub_r2c0);
+}
+
+static inline void haarXY_precheck_boundaries(float* ii_data, int height, int width, int col, int row, int scale, float* haarX, float* haarY) {
+    // (row,col) is upper left corner of haar wavelet filter
+    if (row <= 0 
+        || col <= 0 
+        || (row + 2*scale) > height 
+        || (col + 2*scale) > width) {
+        // wavelet filters that can not be applied completely will be skipped
+        // I think it even makes sense to skip the whole keypoint if its 
+        // descriptor goes over boundaries as it then can not be invariant 
+        return;
+    }
+    
+    // subtracting by one for row/col because row/col is inclusive.
+    int r0 = row - 1;         
+    int c0 = col - 1;         
+    int r1 = row + scale - 1;  
+    int c1 = col + scale - 1;   
+    int r2 = row + 2*scale - 1;
+    int c2 = col + 2*scale - 1;
+
+    float r0c0 = ii_data[r0 * width + c0];
+    float r0c1 = ii_data[r0 * width + c1];
+    float r0c2 = ii_data[r0 * width + c2];
+    float r1c0 = ii_data[r1 * width + c0];
+    float r1c2 = ii_data[r1 * width + c2];
+    float r2c0 = ii_data[r2 * width + c0];
+    float r2c1 = ii_data[r2 * width + c1];
+    float r2c2 = ii_data[r2 * width + c2];
+
+    float r0c0_sub_r2c2 = r0c0 - r2c2;
+    float r0c2_sub_r2c0 = r0c2 - r2c0;
+
+    *haarX = r0c0_sub_r2c2 - 2*(r0c1 - r2c1) + r0c2_sub_r2c0;
+    *haarY = r0c0_sub_r2c2 - 2*(r1c0 - r1c2) - r0c2_sub_r2c0;
+}
+
