5e44a3c8434e7e5e4ded6a9c192c221e3d1a95dc
Sebastian Winberg <winbergs@student.ethz.ch>
2020-05-09 22:06:04 +0200

    Refactored benchmarking framework to compare multiple functions

--- src/benchmark/main_benchmark.cpp
+++ src/benchmark/main_benchmark.cpp
@@ -17,8 +17,10 @@
 #include "stb_image.h"
 
 const char *images[] = {
-    "../images/sunflower/sunflower_32.jpg",  "../images/sunflower/sunflower_64.jpg",
-    "../images/sunflower/sunflower_128.jpg", "../images/sunflower/sunflower_256.jpg"
+    "../images/sunflower/sunflower_32.jpg",  
+    "../images/sunflower/sunflower_64.jpg",
+    //"../images/sunflower/sunflower_128.jpg", 
+    //"../images/sunflower/sunflower_256.jpg"
     //"../images/sunflower/sunflower_512.jpg",
     //"../images/sunflower/sunflower_1024.jpg",
     //"../images/sunflower/sunflower_2048.jpg"
@@ -26,14 +28,14 @@ const char *images[] = {
 };
 #define n_images (sizeof(images) / sizeof(const char *))
 #define BENCHMARK_INTEGRAL_IMAGE
-//#define BENCHMARK_CREATE_RESPONSE_MAP
-//#define BENCHMARK_INTEREST_POINTS
-//#define BENCHMARK_INTERPOLATE_STEPS
-//#define BENCHMARK_GET_DESCRIPTORS
-//#define BENCHMARK_GET_MSURF_DESCRIPTORS
+#define BENCHMARK_CREATE_RESPONSE_MAP
+#define BENCHMARK_INTEREST_POINTS
+#define BENCHMARK_INTERPOLATE_STEPS
+#define BENCHMARK_GET_DESCRIPTORS
+#define BENCHMARK_GET_MSURF_DESCRIPTORS
 
 int main(int argc, char const *argv[]) {
-    std::vector<struct benchmark_data *> all_benchmark_data;
+    std::vector<struct benchmark_data> all_benchmark_data;
     for (int i = 0; i < n_images; i++) {
         char *image_name = (char *)malloc(32 * sizeof(char));
         strcpy(image_name, images[i]);
@@ -57,19 +59,29 @@ int main(int argc, char const *argv[]) {
 
 
 #ifdef BENCHMARK_INTEGRAL_IMAGE
-        printf("compute_integral_img start\n");
-        struct benchmark_data *benchmark_integral_img = initialise_benchmark_data(
-            image_name, width, height, "compute_integral_img", -1, (width + 2 * (height - 1) * width));
-        perf_test_integral_img(compute_integral_img, image, benchmark_integral_img);
-        all_benchmark_data.push_back(benchmark_integral_img);
+        {
+            printf("compute_integral_img start\n");
 
-        /*
-        struct benchmark_data *benchmark_integral_img_opt = initialise_benchmark_data(
-            image_name, width, height, "compute_integral_img_faster_alg", -1, get_flops_compute_integral_img_faster_alg(width, height, 2));
-        perf_test_integral_img(compute_integral_img_faster_alg, image, benchmark_integral_img_opt);
-        all_benchmark_data.push_back(benchmark_integral_img_opt);
-        */
-        printf("compute_integral_img end\n");
+            // Insert all compute_integral_img functions for benchmarking here
+            std::vector<void (*)(float *, int, int, float *)> functions;
+            functions.push_back(compute_integral_img);
+            //functions.push_back(compute_integral_img_faster_alg);
+
+            struct benchmark_data default_data(image_name, width, height, "compute_integral_img", -1, (width + 2 * (height - 1) * width));
+
+            // Insert all respective benchmarking info for compute_integral_img here
+            std::vector<struct benchmark_data> data;
+            data.push_back(default_data);
+            //data.emplace_back(image_name, width, height, "compute_integral_img_faster_alg", -1, get_flops_compute_integral_img_faster_alg(width, height, 2));
+
+            // Benchmarking all compute_integral_img functions and storing timing results in respective entries in data
+            bench_compute_integral_img(functions, image, data);
+            
+            // Appending this data to all benchmarking data
+            all_benchmark_data.insert(all_benchmark_data.end(), data.begin(), data.end());
+
+            printf("compute_integral_img end\n");
+        }
 #endif
 
         // Fast-Hessian
@@ -77,16 +89,17 @@ int main(int argc, char const *argv[]) {
 
         // Create octaves with response layers
         create_response_map(fh);
-
+        /*
 #ifdef BENCHMARK_CREATE_RESPONSE_MAP
-        printf("compute_response_layer start\n");
-        struct benchmark_data *benchmark_compute_response_layer = initialise_benchmark_data(
-            image_name, width, height, "compute_response_layer", -1, (1 + height * width * 13));
-        perf_test_compute_response_layer(compute_response_layer, fh->response_map[0], iimage,
-                                         benchmark_compute_response_layer);
-        all_benchmark_data.push_back(benchmark_compute_response_layer);
-        printf("compute_response_layer end\n");
+        {
+            printf("compute_response_layer start\n");
+            struct benchmark_data data(image_name, width, height, "compute_response_layer", -1, (1 + height * width * 13), data);
+            perf_compute_response_layer(compute_response_layer, fh->response_map[0], iimage, data);
+            all_benchmark_data.push_back(data);
+            printf("compute_response_layer end\n");
+        }
 #endif
+        */
         // Compute responses for every layer
 	    compute_response_map(fh);
 
@@ -95,22 +108,51 @@ int main(int argc, char const *argv[]) {
         get_interest_points(fh, &interest_points);
 
 #ifdef BENCHMARK_INTEREST_POINTS
-        printf("get_interest_points start\n");
-        long flops = 109 * interest_points.size();
-        struct benchmark_data *benchmark_get_interest_points =
-            initialise_benchmark_data(image_name, width, height, "get_interest_points", interest_points.size(), flops);
-        perf_test_get_interest_points(get_interest_points, fh, benchmark_get_interest_points);
-        all_benchmark_data.push_back(benchmark_get_interest_points);
-        printf("get_interest_points end\n");
+        {
+            printf("get_interest_points start\n");
+            
+            // Insert all get_interest_points functions for benchmarking here
+            std::vector<void (*)(struct fasthessian *, std::vector<struct interest_point> *)> functions;
+            functions.push_back(get_interest_points);
+            
+            long flops = 109 * interest_points.size();
+            struct benchmark_data default_data(image_name, width, height, "get_interest_points", interest_points.size(), flops);
+
+            // Insert all respective benchmarking info for get_interest_points here
+            std::vector<struct benchmark_data> data;
+            data.push_back(default_data);
+
+            // Benchmarking all get_interest_point functions and storing timing results in respective entries in data
+            bench_get_interest_points(functions, fh, data);
+            
+            // Appending this data to all benchmarking data
+            all_benchmark_data.insert(all_benchmark_data.end(), data.begin(), data.end());
+            printf("get_interest_points end\n");
+        }
 #endif
 
 #ifdef BENCHMARK_INTERPOLATE_STEPS
-        printf("interpolate_step start\n");
-        struct benchmark_data *benchmark_interpolate_step =
-            initialise_benchmark_data(image_name, width, height, "interpolate_step", interest_points.size(), 109);
-        bench_interpolate_step(fh, benchmark_interpolate_step);
-        all_benchmark_data.push_back(benchmark_interpolate_step);
-        printf("interpolate_step end\n");
+        {
+            printf("interpolate_step start\n");
+            
+            // Insert all interpolate_step functions for benchmarking here
+            std::vector<void (*)(int, int, struct response_layer *, struct response_layer *, struct response_layer *, float[3])> functions;
+            functions.push_back(interpolate_step);
+
+            struct benchmark_data default_data(image_name, width, height, "interpolate_step", interest_points.size(), 109);
+            
+            // Insert all respective benchmarking info for functions here
+            std::vector<struct benchmark_data> data;
+            data.push_back(default_data);
+
+            // Benchmarking all interpolate_step functions and storing timing results in respective entries in data
+            bench_interpolate_step(functions, fh, data);
+
+            // Appending this data to all benchmarking data
+            all_benchmark_data.insert(all_benchmark_data.end(), data.begin(), data.end());
+
+            printf("interpolate_step end\n");
+        }
 #endif
 
 #if !USE_MSURF
@@ -118,12 +160,27 @@ int main(int argc, char const *argv[]) {
         float *GW = get_gaussian(3.3);
 
 #ifdef BENCHMARK_GET_DESCRIPTORS
-        printf("get_descriptor start\n");
-        struct benchmark_data *benchmark_get_descriptor =
-            initialise_benchmark_data(image_name, width, height, "get_descriptor", interest_points.size(), 5734);
-        bench_get_descriptor(iimage, &interest_points, GW, benchmark_get_descriptor);
-        all_benchmark_data.push_back(benchmark_get_descriptor);
-        printf("get_descriptor end\n");
+        {
+            printf("get_descriptor start\n");
+
+            // Insert all interpolate_step functions for benchmarking here
+            std::vector<void (*)(struct integral_image *, struct interest_point *, float *)> functions;
+            functions.push_back(get_descriptor);
+
+            struct benchmark_data default_data(image_name, width, height, "get_descriptor", interest_points.size(), 5734);
+
+            // Insert all respective benchmarking info for functions here
+            std::vector<struct benchmark_data> data;
+            data.push_back(default_data);
+
+            // Benchmarking all get_descriptor functions and storing timing results in respective entries in data
+            bench_get_descriptor(functions, iimage, &interest_points, GW, data);
+
+            // Appending this data to all benchmarking data
+            all_benchmark_data.insert(all_benchmark_data.end(), data.begin(), data.end());
+
+            printf("get_descriptor end\n");
+        }
 #endif
         for (size_t i = 0; i < interest_points.size(); ++i) {
             get_descriptor(iimage, &interest_points[i], GW);
@@ -132,13 +189,28 @@ int main(int argc, char const *argv[]) {
 #else
 
 #ifdef BENCHMARK_GET_MSURF_DESCRIPTORS
-        printf("get_msurf_descriptor start\n");
-        // TODO: (Sebastian) find FLOPS count for get_msurf_descriptor
-        struct benchmark_data *benchmark_get_descriptor =
-            initialise_benchmark_data(image_name, width, height, "get_msurf_descriptor", interest_points.size(), 0);
-        bench_get_msurf_descriptor(iimage, &interest_points, GW, benchmark_get_descriptor);
-        all_benchmark_data.push_back(benchmark_get_descriptor);
-        printf("get_msurf_descriptor end\n");
+        {
+            printf("get_msurf_descriptor start\n");
+            
+            // Insert all interpolate_step functions for benchmarking here
+            std::vector<void (*)(struct integral_image *, struct interest_point *)> functions;
+            functions.push_back(get_msurf_descriptor);
+
+            // TODO: (Sebastian) find FLOPS count for get_msurf_descriptor
+            struct benchmark_data default_data(image_name, width, height, "get_msurf_descriptor", interest_points.size(), -1);            
+
+            // Insert all respective benchmarking info for functions here
+            std::vector<struct benchmark_data> data;
+            data.push_back(default_data);
+
+            // Benchmarking all get_msurf_descriptor functions and storing timing results in respective entries in data
+            bench_get_msurf_descriptor(functions, iimage, &interest_points, data);
+
+            // Appending this data to all benchmarking data
+            all_benchmark_data.insert(all_benchmark_data.end(), data.begin(), data.end());
+
+            printf("get_msurf_descriptor end\n");
+        }
 #endif
 
         // Alternative M-SURF descriptors as in OpenSURF
@@ -163,8 +235,8 @@ int main(int argc, char const *argv[]) {
     save_benchmark_data(all_benchmark_data);
     // free memory benchmarkdata
     // https://stackoverflow.com/questions/10464992/c-delete-vector-objects-free-memory
-    std::vector<struct benchmark_data *>().swap(all_benchmark_data);
-    printf("done\n");
+    //std::vector<struct benchmark_data *>().swap(all_benchmark_data);
+    printf("Benchmarking done!\n");
 
     return 0;
 }
