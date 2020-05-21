7994ca50ea683f499ecc839c5a4379542f3aa720
Carla <cjancik@student.ethz.ch>
2020-05-21 12:35:00 +0200

    added comma

--- src/benchmark/main_benchmark.cpp
+++ src/benchmark/main_benchmark.cpp
@@ -20,7 +20,7 @@
 
 const char *images[] = {
     "../images/sunflower/sunflower_32.jpg",
-    "../images/sunflower/sunflower_64.jpg"
+    "../images/sunflower/sunflower_64.jpg",
     "../images/sunflower/sunflower_128.jpg",
     "../images/sunflower/sunflower_256.jpg",
     "../images/sunflower/sunflower_512.jpg",
