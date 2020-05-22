149f6b761377b75bc5bd622a6177e7393515a67a
Sebastian Winberg <winbergs@student.ethz.ch>
2020-05-22 02:53:52 +0200

    Updated stacked bar plots a bit

--- plots/runtime plots/overall_stacked_bar_plot.py
+++ plots/runtime plots/overall_stacked_bar_plot.py
@@ -23,7 +23,7 @@ outputFileName = 'overall_stacked_bar_plot.png'
 plt.title('Overall Progran Runtime Comparison',  x=-0.1, y=1.05, ha='left', fontsize=16, fontweight='bold')
 
 # Initializing plot axis labels
-plt.xlabel('optimization versions', fontsize=10)
+plt.xlabel('', fontsize=10)
 yl = plt.ylabel('[cycles]', fontsize=10, ha='left')
 yl.set_rotation(0)
 ax.yaxis.set_label_coords(-0.1, 1.01)
@@ -50,14 +50,14 @@ get_msurf_descriptors_cycles = [569110706, 44537542, 53325326]
 
 
 ind = np.arange(N)    # the x locations for the groups
-width = 0.35       # the width of the bars: can also be len(x) sequence
+width = 0.4           # the width of the bars: can also be len(x) sequence
 
 p1 = plt.bar(ind, get_msurf_descriptors_cycles, width, color=colors[0])
 p2 = plt.bar(ind, get_interest_points_cycles, width, bottom=get_msurf_descriptors_cycles, color=colors[1])
 p3 = plt.bar(ind, compute_response_layers_cycles, width, bottom=np.array(get_msurf_descriptors_cycles)+np.array(get_interest_points_cycles), color=colors[2])
 p4 = plt.bar(ind, compute_integral_image_cycles, width, bottom=np.array(get_msurf_descriptors_cycles)+np.array(get_interest_points_cycles)+np.array(compute_response_layers_cycles), color=colors[3])
 
-plt.xticks(ind, ('Base', 'Optimized\n(not padded)', 'Optimized\n(padded)'))
+plt.xticks(ind, ('base', 'optimized\n(not padded)', 'optimized\n(padded)'))
 plt.yticks(np.arange(0, 800000001, 100000000))
 plt.legend((p4[0], p3[0], p2[0], p1[0]), plotLabels)
 #plt.legend((p1[0], p2[0], p3[0]), ('1', '2', '3'))
