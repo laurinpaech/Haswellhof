8940165d937af11a8bddedb961f94eff5fa24688
@volflow <valentin@valentin-wolf.de>
2020-06-12 12:12:21 +0200

    autotune for laurin

--- src/autotune/main_autotune.cpp
+++ src/autotune/main_autotune.cpp
@@ -19,12 +19,12 @@
 
 const char *images[] = {
     // "../images/sunflower/sunflower_32.jpg",  
-    "../images/sunflower/sunflower_64.jpg",
-    "../images/sunflower/sunflower_128.jpg", 
-    "../images/sunflower/sunflower_256.jpg",
-    "../images/sunflower/sunflower_512.jpg",
+    // "../images/sunflower/sunflower_64.jpg",
+    // "../images/sunflower/sunflower_128.jpg", 
+    // "../images/sunflower/sunflower_256.jpg",
+    // "../images/sunflower/sunflower_512.jpg",
     "../images/sunflower/sunflower_1024.jpg",
-    "../images/sunflower/sunflower_2048.jpg",
+    // "../images/sunflower/sunflower_2048.jpg",
     // "../images/sunflower/sunflower_4096.jpg",
 };
 
